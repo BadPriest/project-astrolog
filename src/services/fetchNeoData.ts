afdd30c2398a93ee22caf348485d73ccdbf422bb
Bruno V. Vergatti <bruno_vergatti@yahoo.com.br>
2022-10-25 00:51:39 +0300

    Creates a query queue based on user input
    - If user inputs a range within the 7 days limit, only 1 entry will be put in queue
    - If user inputs larger range, than system will break that down into pairs and build the urls for each request

--- src/services/fetchNeoData.ts
+++ src/services/fetchNeoData.ts
@@ -1,3 +1,4 @@
+import { add, differenceInCalendarDays } from "date-fns";
 import ENDPOINTS from "../core/api/endpoints";
 import { makeURL } from "../core/api/utils";
 import parseDateForAPI from "../utils/parseDates";
@@ -7,22 +8,67 @@ export interface IQueryNEOData {
   endDate: Date;
 }
 
+export const getQueryQueue = (originalQuery: IQueryNEOData) => {
+  const queue = [] as IQueryNEOData[];
+
+  let cursorStart = originalQuery.startDate;
+  let cursorEnd = originalQuery.endDate;
+
+  do {
+    const diffInDays = differenceInCalendarDays(cursorEnd, cursorStart);
+
+    if (diffInDays <= 7) {
+      const newQuery = {
+        startDate: cursorStart,
+        endDate: cursorEnd,
+      } as IQueryNEOData;
+
+      queue.push(newQuery);
+    }
+
+    if (diffInDays > 7) {
+      cursorEnd = add(cursorStart, { days: 7 });
+
+      const newQuery = {
+        startDate: cursorStart,
+        endDate: cursorEnd,
+      } as IQueryNEOData;
+
+      cursorStart = add(cursorEnd, { days: 1 });
+      cursorEnd = originalQuery.endDate;
+      queue.push(newQuery);
+    }
+  } while (!queue.find((e) => e.endDate === originalQuery.endDate));
+
+  return queue;
+};
+
+const getURLQueue = (queryQueue: IQueryNEOData[]) => {
+  const queue = [] as URL[];
+
+  queryQueue.forEach((entry) => {
+    const newUrl = makeURL({
+      endpoint: ENDPOINTS.LIST,
+      params: new Map<string, string>([
+        ["start_date", parseDateForAPI(entry.startDate)],
+        ["end_date", parseDateForAPI(entry.endDate)],
+      ]),
+    });
+
+    queue.push(newUrl);
+  });
+
+  return queue;
+};
+
 const fetchNEOData = (query: IQueryNEOData) => {
   const { startDate, endDate } = query;
 
-  const url = makeURL({
-    endpoint: ENDPOINTS.LIST,
-    params: new Map<string, string>([
-      ["start_date", parseDateForAPI(startDate)],
-      ["end_date", parseDateForAPI(endDate)],
-    ]),
-  });
+  const queryQueue = getQueryQueue(query);
+  const urlQueue = getURLQueue(queryQueue);
 
-  // TODO: Assemble request queue
   // TODO: Execute requests (parallel, serial, test)
   // TODO: Normalize responses
-  
-  console.log("url", url);
 };
 
 export default fetchNEOData;
