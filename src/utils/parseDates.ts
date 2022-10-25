494729ca3705d2baf18d18bd78bec2d0e10f64c1
Bruno V. Vergatti <bruno_vergatti@yahoo.com.br>
2022-10-26 00:56:54 +0300

    Adds parser for display dates

--- src/utils/parseDates.ts
+++ src/utils/parseDates.ts
@@ -3,6 +3,15 @@ import { format, parse } from "date-fns";
 export const parseDateForAPI = (date: Date, formatTo = "yyyy-MM-dd") =>
   format(date, formatTo);
 
+export const parseDateForDisplay = (
+  date: string,
+  formatIs = "yyyy-MM-dd",
+  formatTo = "dd.MM.yyyy"
+) => {
+  const parsed = parseDate(date, formatIs);
+  return format(parsed, formatTo);
+};
+
 export const parseDate = (date: string, formatIs = "dd.MM.yyyy") =>
   parse(date, formatIs, new Date());
 
