932c42d25a1a99dba7124bd2508df50d73468f71
Bruno V. Vergatti <bruno_vergatti@yahoo.com.br>
2022-11-06 23:36:48 +0200

    Fix typo, quotes

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -1,52 +1,53 @@
-import { IError } from "../state/models/error";
+import { IError } from '../state/models/error';
 
 export const ERRORS = {
   INPUT: {
     INCOMPLETE: {
-      code: "001",
-      message: "DATE_INCOMPLETE: Insert full date - try 01.01.1900",
+      code: '001',
+      message: 'DATE_INCOMPLETE: Insert full date - try 01.01.1900',
     } as IError,
     INVALID_DATE: {
-      code: "002",
-      message: "DATE_INVALID: Input is not recognizable as a true date",
+      code: '002',
+      message: 'DATE_INVALID: Input is not recognizable as a true date',
     } as IError,
   },
   FORM: {
     INVALID: {
-      code: "003",
-      message: "FORM_INVALID: Information is not properly arranged.",
+      code: '003',
+      message: 'FORM_INVALID: Information is not properly arranged.',
       avoidFeedback: true,
     } as IError,
     EMPTY: {
-      code: "004",
-      message: "FORM_EMPTY: Both inputs are empty",
+      code: '004',
+      message: 'FORM_EMPTY: Both inputs are empty',
       avoidFeedback: true,
     } as IError,
     IMPOSSIBLE_RANGE: {
-      code: "005",
-      message: "IMPOSSIBLE_RANGE: The final date should come after the initial",
+      code: '005',
+      message: 'IMPOSSIBLE_RANGE: The final date should come after the initial',
     } as IError,
     EXCEEDED_RANGE: {
-      code: "007",
-      message: "EXCEEDED_RANGE: Date range cannot exceed 180 days at a time",
+      code: '007',
+      message: 'EXCEEDED_RANGE: Date range cannot exceed 180 days at a time',
     },
   },
   DATA: {
     UNEXPECTED: {
-      code: "006",
-      message: "UNEXPECTED_DATA: The data is not what we expected",
+      code: '006',
+      message: 'UNEXPECTED_DATA: The data is not what we expected',
     } as IError,
   },
-  REQUEST: { 
+  REQUEST: {
     UNEXPECTED_RESPONSE: {
-      code: "008",
-      message: 'UNEXPECTED_RESPONSE: Server unswer in an unexpected manner'
+      code: '008',
+      message: 'UNEXPECTED_RESPONSE: Server responded in an unexpected manner',
     } as IError,
     EXCEEDED_API_USAGE: {
       code: '009',
-      message: 'EXCEEDED_API_USAGE: Data access expired due to excessive usage. Please try again later'
-     } as IError,
-  }
+      message:
+        'EXCEEDED_API_USAGE: Data access expired due to excessive usage. Please try again later',
+    } as IError,
+  },
 };
 
 export default ERRORS;
